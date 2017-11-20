579f347bf1e35992938d4aefc76f4290aca0bb1c
Alex Seceleanu <alexseceleanu@gmail.com>
2017-11-20 23:28:22 +0200

    Create TapeEquilibrium.cpp
    A non-empty zero-indexed array A consisting of N integers is given. Array A represents numbers on a tape.
    
    Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
    
    The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
    
    In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
    
    For example, consider array A such that:
    
      A[0] = 3
      A[1] = 1
      A[2] = 2
      A[3] = 4
      A[4] = 3
    We can split this tape in four places:
    
    P = 1, difference = |3 − 10| = 7 
    P = 2, difference = |4 − 9| = 5 
    P = 3, difference = |6 − 7| = 1 
    P = 4, difference = |10 − 3| = 7 
    Write a function:
    
    int solution(vector<int> &A);
    
    that, given a non-empty zero-indexed array A of N integers, returns the minimal difference that can be achieved.
    
    For example, given:
    
      A[0] = 3
      A[1] = 1
      A[2] = 2
      A[3] = 4
      A[4] = 3
    the function should return 1, as explained above.
    
    Assume that:
    
    N is an integer within the range [2..100,000];
    each element of array A is an integer within the range [−1,000..1,000].
    Complexity:
    
    expected worst-case time complexity is O(N);
    expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
    Elements of input arrays can be modified.
    
    Copyright 2009–2017 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.

---
+++ TapeEquilibrium.cpp
@@ -0,0 +1,32 @@
+ #include <algorithm>
+ #include <iostream>
+ #include <vector>
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) 
+{
+    unsigned int i, l = A.size();
+    long long sum = 0;
+    long long leftSum =0;
+    int  min = 999999, minAux;
+    
+    //find the sum of all elements
+    for (i = 0 ; i < l; i++ )
+        sum += A[i];
+    
+    //search for the minimal difference
+    for (i = 0 ; i < l; i++ )
+    {   
+        leftSum += A[i];
+        sum -= A[i];
+        //compute absolute difference
+        if ( sum > leftSum )
+            minAux =  sum - leftSum;
+        else minAux = leftSum - sum; 
+        
+        if( minAux < min )
+            min = minAux;
+    }        
+    return min;   
+}
