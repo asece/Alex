2596a9c38dc495c18f7571869ec2b511350435dc
Alex Seceleanu <alexseceleanu@gmail.com>
2017-11-20 21:58:57 +0200

    Create OddOccurrencesInArray.cpp

---
+++ OddOccurrencesInArray.cpp
@@ -0,0 +1,18 @@
+// you can use includes, for example:
+// #include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) 
+{
+    int i, odd = 0;
+    for ( i = 0 ; i< A.size() ; i++ )
+    {
+        odd = odd ^ A[i];
+    }
+    return odd;
+    
+}
