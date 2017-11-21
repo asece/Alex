e54b4af57c0c96f8a9d0216b434e92d39f4fa436
Alex Seceleanu <alexseceleanu@gmail.com>
2017-11-21 12:45:42 +0200

    Create  PermMissingElem.cpp
    A zero-indexed array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
    
    Your goal is to find that missing element.
    
    Write a function:
    
        int solution(vector<int> &A);
    
    that, given a zero-indexed array A, returns the value of the missing element.
    
    For example, given array A such that:
      A[0] = 2
      A[1] = 3
      A[2] = 1
      A[3] = 5
    
    the function should return 4, as it is the missing element.
    
    Assume that:
    
            N is an integer within the range [0..100,000];
            the elements of A are all distinct;
            each element of array A is an integer within the range [1..(N + 1)].
    
    Complexity:
    
            expected worst-case time complexity is O(N);
            expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
    
    Elements of input arrays can be modified.

---
+++ PermMissingElem.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+ #include <vector>
+ #include <iostream>
+ using namespace std;
+ 
+int solution(vector<int> &A) {
+    int missing = 0;
+    
+    //first computation
+    for (int i = 0 ; i< int(A.size()) ; i++)
+        missing  += (i + 1 - A[i]);
+    //find the missing element
+    missing += int(A.size() + 1);
+    
+    return missing;
+}
