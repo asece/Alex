527500dccced51f628d7326a5c985bca24220838
Alex Seceleanu <alexseceleanu@gmail.com>
2017-11-21 21:59:36 +0200

    Create SingleSwapable.cpp

---
+++ SingleSwapable.cpp
@@ -0,0 +1,28 @@
+// you can use includes, for example:
+ #include <algorithm>
+ #include <vector>
+ #include <iostream>
+ using namespace std;
+ 
+ 
+bool solution(vector<int> &A) 
+{
+    int i, event = 0;
+    
+    // for (i = 0; i < int(A.size()-1); i++ )
+    //     if (A[i] > A[i+1])
+    //         event++;
+    // if (event == 1 && A[i+1]!=A[i]) 
+    //     return 0;
+    
+    for( i = int(A.size()); i >0; i--)
+        if (A[i] == A[i-1]) 
+            i--;
+        else if (A[i] < A[i-1])
+            event++;
+            
+    if (event > 1)
+        return 0;
+    if (event < 2)
+        return 1;
+}
