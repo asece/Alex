8be8fdaa8751b0a16857084f22a2275205f78bd3
asece <alexseceleanu@gmail.com>
2020-03-02 12:30:07 +0200

    Automated Sanity Check

--- Composition101.cpp
+++ Composition101.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-02-28
+//  Last sanity check: 2020-03-02
 // Example program
 #include <iostream>
 #include <string>
--- WordMachine.cpp
+++ WordMachine.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-02-28
+//  Last sanity check: 2020-03-02
 // you can use includes, for example:
  #include <algorithm>
  #include <iostream>
