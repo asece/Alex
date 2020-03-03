73e0a8f759aff0bd401bf29fbf03fe6cfb6c3ff7
asece <alexseceleanu@gmail.com>
2020-03-03 13:36:12 +0200

    Automated Sanity Check

--- StaticPolymorphism.cpp
+++ StaticPolymorphism.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-03-02
+//  Last sanity check: 2020-03-03
 // Example program
 #include <iostream>
 #include <string>
--- removeAlternatingCharacters.cpp
+++ removeAlternatingCharacters.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-03-02
+//  Last sanity check: 2020-03-03
 int alternatingCharacters(string s) 
 {
     int del = 0, i = 0;
