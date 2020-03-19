8ac1fe0ab56463d0f1346dbf4c5b47eede40da79
asece <alexseceleanu@gmail.com>
2020-03-19 10:33:28 +0200

    Automated Sanity Check

--- CountingValleys.cpp
+++ CountingValleys.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-03-18
+//  Last sanity check: 2020-03-19
 //https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
 
 #include <bits/stdc++.h>
--- ResourceManagementDeepCopy.cpp
+++ ResourceManagementDeepCopy.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-03-18
+//  Last sanity check: 2020-03-19
 // Resource Management
 // persons.push_back(Person("Alex")) trebuie facut doar daca exista copy contructor si operator definite!
 // altfel se face copie shallow si programul nu merge! 
