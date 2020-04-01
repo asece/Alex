efd6a8244ba320a9ac382919546254afd1659ad7
asece <alexseceleanu@gmail.com>
2020-04-01 10:35:09 +0300

    Automated Sanity Check

--- LongestIncreasingSequenceDynamic.cpp
+++ LongestIncreasingSequenceDynamic.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-03-31
+//  Last sanity check: 2020-04-01
 // Longest Increasing Sequence - dynamic 
 #include <iostream>
 #include <string>
--- Multiple Inheritance.cpp
+++ Multiple Inheritance.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-03-31
+//  Last sanity check: 2020-04-01
 // Example program
 #include <iostream>
 #include <string>
