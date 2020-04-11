59d4bddf3d4e2dbbce1ba1e0534d456d4e45e867
asece <alexseceleanu@gmail.com>
2020-04-11 10:34:43 +0300

    Automated Sanity Check

--- lists.cpp
+++ lists.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-10
+//  Last sanity check: 2020-04-11
 //
 //  main.cpp
 //  Lists
--- writeReadBinaries.cpp
+++ writeReadBinaries.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-10
+//  Last sanity check: 2020-04-11
 //
 //  main.cpp
 //  Write and read binary files
