94068aba60b1ec3786eafbd0fe1e17f1abda0344
asece <alexseceleanu@gmail.com>
2020-04-14 10:35:33 +0300

    Automated Sanity Check

--- Anagram.cpp
+++ Anagram.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-13
+//  Last sanity check: 2020-04-14
 
 int makeAnagram(string a, string b) {
 
--- FindANumberInArray.cpp
+++ FindANumberInArray.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-13
+//  Last sanity check: 2020-04-14
 // C++ implementation below
 #include <iostream>
 #include <sys/resource.h>
