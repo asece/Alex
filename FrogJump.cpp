c5fe5982a0aab0bb8225c7fbc7f48c8914d696d8
asece <alexseceleanu@gmail.com>
2020-04-17 10:34:55 +0300

    Automated Sanity Check

--- FrogJump.cpp
+++ FrogJump.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-16
+//  Last sanity check: 2020-04-17
 int solution(int X, int Y, int D) 
 {
     int jumps = (Y-X) / D;
--- LongestCommonSubsequenceDynamic.cpp
+++ LongestCommonSubsequenceDynamic.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-16
+//  Last sanity check: 2020-04-17
 // Longest Common Subsequence - dynamic
 // X = ACCG
 // Y = CCAGCA
