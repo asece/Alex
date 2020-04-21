4cc5a640b175ec948e7548e25645a6d291b390fa
asece <alexseceleanu@gmail.com>
2020-04-21 10:35:32 +0300

    Automated Sanity Check

--- FindANumberInArray.cpp
+++ FindANumberInArray.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-20
+//  Last sanity check: 2020-04-21
 // C++ implementation below
 #include <iostream>
 #include <sys/resource.h>
--- ResourceManagementDeepCopy.cpp
+++ ResourceManagementDeepCopy.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-20
+//  Last sanity check: 2020-04-21
 // Resource Management
 // persons.push_back(Person("Alex")) trebuie facut doar daca exista copy contructor si operator definite!
 // altfel se face copie shallow si programul nu merge! 
