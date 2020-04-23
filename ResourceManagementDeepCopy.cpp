5dce6992e712e4b9344ff4ddc966ae60bc23f686
asece <alexseceleanu@gmail.com>
2020-04-23 10:35:43 +0300

    Automated Sanity Check

--- ResourceManagementDeepCopy.cpp
+++ ResourceManagementDeepCopy.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-04-22
+//  Last sanity check: 2020-04-23
 // Resource Management
 // persons.push_back(Person("Alex")) trebuie facut doar daca exista copy contructor si operator definite!
 // altfel se face copie shallow si programul nu merge! 
