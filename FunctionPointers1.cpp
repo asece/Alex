34c695e578bfdde62879ef2fc230e8ca61e85289
asece <alexseceleanu@gmail.com>
2020-05-06 10:34:57 +0300

    Automated Sanity Check

--- FunctionPointers1.cpp
+++ FunctionPointers1.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-05
+//  Last sanity check: 2020-05-06
 #include <stdio.h>
 #include <iostream>
 #include <vector>
--- OddOccurrencesInArray.cpp
+++ OddOccurrencesInArray.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-05
+//  Last sanity check: 2020-05-06
 // you can use includes, for example:
 // #include <algorithm>
 #include <iostream>
