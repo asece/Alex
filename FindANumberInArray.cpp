81fd110e5a2c3f4270c287aa3aad47cff1ab33dc
asece <alexseceleanu@gmail.com>
2020-05-07 10:35:22 +0300

    Automated Sanity Check

--- FindANumberInArray.cpp
+++ FindANumberInArray.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-06
+//  Last sanity check: 2020-05-07
 // C++ implementation below
 #include <iostream>
 #include <sys/resource.h>
--- TapeEquilibrium.cpp
+++ TapeEquilibrium.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-06
+//  Last sanity check: 2020-05-07
  #include <algorithm>
  #include <iostream>
  #include <vector>
