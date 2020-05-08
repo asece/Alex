65acf09ed9326249b945666364a1f6c4d7cd226d
asece <alexseceleanu@gmail.com>
2020-05-08 10:35:25 +0300

    Automated Sanity Check

--- Composition101.cpp
+++ Composition101.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-07
+//  Last sanity check: 2020-05-08
 // Example program
 #include <iostream>
 #include <string>
--- removeAlternatingCharacters.cpp
+++ removeAlternatingCharacters.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-07
+//  Last sanity check: 2020-05-08
 int alternatingCharacters(string s) 
 {
     int del = 0, i = 0;
