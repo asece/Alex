87794382e8160325b13c5755ae4e19828d397452
asece <alexseceleanu@gmail.com>
2020-05-16 10:34:45 +0300

    Automated Sanity Check

--- basic_exceptions.cpp
+++ basic_exceptions.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-15
+//  Last sanity check: 2020-05-16
 //
 //  Created by Alex Seceleanu on 11/20/17.
 //  Copyright © 2017 Alex Seceleanu. All rights reserved.
--- vector size and cap.cpp
+++ vector size and cap.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-15
+//  Last sanity check: 2020-05-16
 //  main.cpp
 //  Exer
 //
