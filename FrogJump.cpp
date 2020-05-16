92e18f146ff9af4c4a36ffa082f78ba0e84b674b
asece <alexseceleanu@gmail.com>
2020-05-16 10:35:51 +0300

    Automated Sanity Check

--- FrogJump.cpp
+++ FrogJump.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-15
+//  Last sanity check: 2020-05-16
 int solution(int X, int Y, int D) 
 {
     int jumps = (Y-X) / D;
--- LongestIncreasingSequenceDynamic.cpp
+++ LongestIncreasingSequenceDynamic.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-15
+//  Last sanity check: 2020-05-16
 // Longest Increasing Sequence - dynamic 
 #include <iostream>
 #include <string>
