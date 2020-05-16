d3927c4987b06f230de36a9225baec21ef853188
asece <alexseceleanu@gmail.com>
2020-05-16 10:35:25 +0300

    Automated Sanity Check

--- FindANumberInArray.cpp
+++ FindANumberInArray.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-15
+//  Last sanity check: 2020-05-16
 // C++ implementation below
 #include <iostream>
 #include <sys/resource.h>
--- removeAlternatingCharacters.cpp
+++ removeAlternatingCharacters.cpp
@@ -1,4 +1,4 @@
-//  Last sanity check: 2020-05-15
+//  Last sanity check: 2020-05-16
 int alternatingCharacters(string s) 
 {
     int del = 0, i = 0;
